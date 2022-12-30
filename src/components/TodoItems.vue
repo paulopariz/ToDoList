edbe13ce2bfbea27dba8b54e2bebb427c778c424
paulopariz <paulopariz01@gmail.com>
2022-12-30 15:46:31 -0300

    components 🚀

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div class="space-y-2">
+    
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    
+  </div>
+
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+  export default { components: { TodoItem } }
+</script>
